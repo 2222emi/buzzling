1ca3413bece75983f9b8456ea038bd35a97beb30
2222emi <im.nagayama.emi@gmail.com>
2021-02-20 23:34:09 +0900

    Add validates(空白投稿なし)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,14 +8,14 @@ def new
 end
 
 def create
-  Tweet.create(text: params[:text] ,image: params[:image] )
+  # Tweet.create(text: params[:text] ,image: params[:image] )
+  Tweet.create(tweet_params)
   
 end
 
 private
 def tweet_params
-  params.permimt(:text , :image)
+  params.permit(:name, :text , :image)
   
 end
-
 end
