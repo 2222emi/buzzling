2b6312eff5882f432a493a3b0dd2cfb04ee0ed4b
2222emi <im.nagayama.emi@gmail.com>
2021-03-07 19:45:13 +0900

    アソシエーション(tweet,user)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_093553) do
+ActiveRecord::Schema.define(version: 2021_03_07_034250) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "comment"
+    t.string "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
